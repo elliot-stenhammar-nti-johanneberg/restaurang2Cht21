3655f2479c5c8a97648463f4a2d119bed5dab192
elliot-stenhammar-nti-johanneberg <elliot.markstromstenhammar@elev.ga.ntig.se>
2021-12-20 11:47:05 +0100

    skisssar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-var homebtn = document.querySelectorAll("a")[0];
-var menubtn = document.querySelectorAll("a")[1];
-var aboutbtn = document.querySelectorAll("a")[2];
+var homeBtn = document.querySelectorAll("a")[1];
+var menuBtn = document.querySelectorAll("a")[2];
+var aboutBtn = document.querySelectorAll("a")[3];
+
+var currentUrl = window.location.href;
+
+if (currentUrl.endsWith("index.html")) {
+    homeBtn.style.opacity = 1;
+} else if (currentUrl.endsWith("menu.html")) {
+    menuBtn.style.opacity = 1;
+} else {
+    aboutBtn.style.opacity = 1;
+}
